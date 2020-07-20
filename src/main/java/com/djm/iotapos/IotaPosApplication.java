734b9fbd71427567deb8a6cde8f5c95b8cfcb211
David Mordigal <davidmordigal@gmail.com>
2020-07-20 18:28:32 -0400

    initial project commit, update license

---
+++ src/main/java/com/djm/iotapos/IotaPosApplication.java
@@ -0,0 +1,13 @@
+package com.djm.iotapos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotaPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotaPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djm/iotapos/IotaPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djm.iotapos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotaPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
